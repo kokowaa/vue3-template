5cba3941663244167b9843b689628876555b666d
Jungyin3 <jungyin316133356@163.com>
2023-02-06 09:07:58 +0800

    type: 修复登录模块类型提示

--- src/api/modules/login.ts
+++ src/api/modules/login.ts
@@ -1,7 +1,7 @@
 import http from "@/api";
 import { Login } from "../interface";
 
-// 登录
+/** 登录 */
 export const loginAPI = (params: Login.ReqLogin) => {
   return http.post<Login.ResLogin>("/login", params);
 };
